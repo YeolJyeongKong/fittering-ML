86e515fa77ee81f1dce2d50e879445e0beb94774
sinjy1203 <sinjy1203@gmail.com>
2023-06-21 18:43:46 +0900

    check synthetic data

--- modeling/smplx_visualize.py
+++ modeling/smplx_visualize.py
@@ -1,10 +1,12 @@
 import torch
 import os
+import numpy as np
 
 from smplpytorch.pytorch.smpl_layer import SMPL_Layer
 from smplpytorch.display_utils import display_model
 # from STRAPS_3DHumanShapePose.data.synthetic_training_dataset import SyntheticTrainingDataset
 
+neutral = np.load("/home/shin/VScodeProjects/fittering-ML/modeling/STRAPS-3DHumanShapePose/additional/neutral_smpl_mean_params_6dpose.npz", mmap_mode='r')
 
 if __name__ == '__main__':
     print(os.getcwd)
@@ -14,24 +16,15 @@ if __name__ == '__main__':
     # Create the SMPL layer
     smpl_layer = SMPL_Layer(
         center_idx=0,
-        gender='female',
-        model_root='./smplpytorch/smplpytorch/native/models')
+        gender='neutral',
+        model_root='/home/shin/VScodeProjects/fittering-ML/modeling/smplpytorch/smplpytorch/native/models')
 
     # Generate random pose and shape parameters
     # pose_params = torch.rand(batch_size, 72) * 0.2
     # shape_params = torch.rand(batch_size, 10) * 0.03
 
     pose_params = torch.zeros((1, 72))
-    shape_params = torch.tensor([[0.04792313692484085,
-        0.26527075040151804,
-        0.7512205727618557,
-        0.2978821327866097,
-        0.1866084982418149,
-        0.3341038157933196,
-        0.8757696215861747,
-        0.09900752184889527,
-        0.7856228425535512,
-        0.7729162775177757]])
+    shape_params = torch.tensor(neutral['shape']).view(1, -1)
 
     # dataset = SyntheticTrainingDataset(npz_path="./STRAPS_3DHumanShapePose/data/amass_up3d_3dpw_train.npz", 
     #                                    params_from='3dpw') #['all', 'h36m', 'up3d', '3dpw', 'not_amass']
@@ -56,6 +49,6 @@ if __name__ == '__main__':
         model_faces=smpl_layer.th_faces,
         with_joints=True,
         kintree_table=smpl_layer.kintree_table,
-        # savepath='image.png',
-        savepath=None,
+        savepath='/home/shin/VScodeProjects/fittering-ML/modeling/image.png',
+        # savepath=None,
         show=True)
