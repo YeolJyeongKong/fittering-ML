fe7dbd355b7893b1edd2576c86b712feaec8444d
sinjy1203 <sinjy1203@gmail.com>
2023-07-05 00:11:14 +0900

    feat: inference module for docker deploy

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -4,27 +4,10 @@ import torchvision.transforms.functional as F
 import numpy as np
 import torch.nn.functional as nn_f
 
-class TrueCrop(object):
-    def __init__(self, di) -> None:
-        self.di = di
-    def __call__(self, image) -> Any:
-        image = image.cpu().type(torch.FloatTensor)
-        assert image.ndim == 3, f"image.ndim must be 3 but get {image.ndim}"
-        assert image.shape[0] == 1, "image must be binary image"
-        _, y, x = image.nonzero(as_tuple=True)
-        y = torch.sort(y)[0]
-        x = torch.sort(x)[0]
-        if self.di == 0:
-            image = image[:, y[0]:y[-1], x[0]:x[-1]]
-        elif self.di == 1:
-            image = image[:, y[0]:y[-1], :]
-        else:
-            raise Exception("di must be in (0, 1)")
-        return image
     
-def crop_true(image, di):
+def crop_true(image):
     image = image.cpu().type(torch.FloatTensor)
-    assert image.ndim == 3, f"image.ndim must be 3 but get {image.ndim}"
+    assert image.ndim == 3, f'image.ndim must be 3 but get {image.ndim}'
     assert image.shape[0] == 1, "image must be binary image"
     _, y, x = image.nonzero(as_tuple=True)
     y = torch.sort(y)[0]
@@ -42,18 +25,23 @@ def crop_true(image, di):
     padded_image = nn_f.pad(image, pad=padding_width+padding_height)
     return padded_image
 
-def crop_resize_binary_image(image: torch.Tensor, resize_wh=512) -> torch.Tensor:
-    assert image.ndim == 3, f"image.ndim must be 3 but get {image.ndim}"
-    _, y, x = image.nonzero(as_tuple=True)
-    y = torch.sort(y)[0]
-    x = torch.sort(x)[0]
-    image = image[:, y[0]:y[-1], x[0]:x[-1]]
-    image = F.resize(image, size=512)
-    print(image.shape)
-    # image = F.to_pil_image(image.cpu().type(torch.FloatTensor))
-    # image = F.resize(image, (512, 512))
-    # image = totensor(np.array(image))
-    return image
+def convert_multiclass_to_binary_labels_torch(multiclass_labels):
+    """
+    Converts multiclass segmentation labels into a binary mask.
+    """
+    binary_labels = torch.zeros_like(multiclass_labels)
+    binary_labels[multiclass_labels != 0] = 1
+
+    return binary_labels
+
+def binary_labels_torch(multiclass_labels):
+    """
+    Converts multiclass segmentation labels into a binary mask.
+    """
+    binary_labels = torch.zeros_like(multiclass_labels)
+    binary_labels[multiclass_labels > 0.5] = 1
+
+    return binary_labels
 
 def PILfromtensor(tensor: torch.Tensor):
     return tensor.detach().cpu().numpy()
---
+++ predict.py
@@ -0,0 +1,54 @@
+from PIL import Image
+import numpy as np
+import torch
+import torch.nn as nn
+from lightning_modules import CNNForwardModule
+from datamodule import DataModule
+import torchvision.transforms.functional as F
+
+from utils.predict import Beta2Measurements
+
+import sys
+sys.path.append("/home/shin/VScodeProjects/fittering-ML/opensrc/SemanticGuidedHumanMatting")
+from opensrc.SemanticGuidedHumanMatting.model.model import HumanSegment, HumanMatting
+from opensrc.SemanticGuidedHumanMatting import utils
+from opensrc.SemanticGuidedHumanMatting import inference
+
+def predict(front_image, side_image, height, 
+            model_cnn_ckpt_path='/home/shin/VScodeProjects/fittering-ML/wandb-lightning/ktb7gxc2/checkpoints/epoch=19-step=10000.ckpt', 
+            segmentation_ckpt_path='/home/shin/VScodeProjects/fittering-ML/opensrc/SemanticGuidedHumanMatting/pretrained/SGHM-ResNet50.pth'):
+    model_cnn = CNNForwardModule(device=torch.device('cpu')).\
+        load_from_checkpoint(model_cnn_ckpt_path)
+    model_cnn.eval()
+
+    model = HumanMatting(backbone='resnet50')
+    model = nn.DataParallel(model).eval()
+    model.load_state_dict(torch.load(segmentation_ckpt_path))
+
+    front_pred_alpha, front_pred_mask = inference.single_inference(model, front_image)
+    side_pred_alpha, side_pred_mask = inference.single_inference(model, side_image)
+
+
+    dm = DataModule(batch_size=1)
+
+    front_bin_image = torch.tensor(front_pred_alpha[np.newaxis, ...])
+    side_bin_image = torch.tensor(side_pred_alpha[np.newaxis, ...])
+    front = dm.transform(front_bin_image)
+    side = dm.transform(side_bin_image)
+
+    image = torch.cat((front, side), dim=0).reshape(1, 2, 512, 512)
+
+    with torch.no_grad():
+        pred = model_cnn(image, torch.tensor(height))
+
+    beta2meas = Beta2Measurements(device=torch.device("cpu"))
+    meas = beta2meas.predict(pred)
+    
+    return meas
+
+
+if __name__ == "__main__":
+    front_bin_image = Image.open("/home/shin/VScodeProjects/fittering-ML/opensrc/SemanticGuidedHumanMatting/image_data/IMG_4251.jpg")
+    side_bin_image = Image.open("/home/shin/VScodeProjects/fittering-ML/opensrc/SemanticGuidedHumanMatting/image_data/IMG_4254.jpg")
+    meas = predict(front_bin_image, side_bin_image, 1.81)
+    print(meas)
