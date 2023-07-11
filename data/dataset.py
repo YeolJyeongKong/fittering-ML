b8f29052772453947b3b3c9a6ded8df602f2bc5c
sinjy1203 <sinjy1203@gmail.com>
2023-07-11 14:50:23 +0900

    feat: transform

--- data/dataset.py
+++ data/dataset.py
@@ -9,17 +9,17 @@ import torch
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset
 from torchvision import transforms
+from torchvision.transforms import ToTensor, Lambda, ToPILImage, Resize
+from torchvision.transforms import functional as F
 
 import config
 
-from data.augmentation import AugmentBetasCam
 from data.preprocessing import *
 
-class BinaryImageBetaDataset(Dataset):
-    def __init__(self, data_dir, transform=None, augment=None) -> None:
+class BinaryImageMeasDataset(Dataset):
+    def __init__(self, data_dir, transform=None) -> None:
         super().__init__()
         self.transform = transform
-        self.augment = augment
 
         self.image_dir = os.path.join(data_dir, 'images')
         json_dir = os.path.join(data_dir, 'json')
@@ -37,32 +37,25 @@ class BinaryImageBetaDataset(Dataset):
         height = torch.tensor(json_data['height'])
         measurement_lst = torch.tensor([json_data[mea_name] for mea_name in config.MEASUREMENTS_ORDER])
 
-        if self.transform:
-            front_image = self.transform(front_image)
-            side_image = self.transform(side_image)
+        front_arr = np.array(front_pil)
+        side_arr = np.array(side_pil)
 
-        return {'front_image': front_image, 
-                'side_image': side_image, 
+        if self.transform:
+            front = self.transform(front_arr)
+            side = self.transform(side_arr)
+
+        return {'front': front, 
+                'side': side, 
                 'height': height, 
                 'meas': measurement_lst}
 
 if __name__ == "__main__":
-    os.chdir("/home/shin/VScodeProjects/fittering-ML")
-    data_transforms = transforms.Compose([
-        transforms.Lambda(crop_true),
-        transforms.ToPILImage(),
-        transforms.Resize((512, 512)), 
-        transforms.ToTensor(),
-        transforms.Lambda(convert_multiclass_to_binary_labels_torch)
-    ])
-    dataset = BinaryImageBetaDataset(ord_data_path="/home/shin/VScodeProjects/fittering-ML/modeling/STRAPS-3DHumanShapePose/data/amass_up3d_3dpw_train.npz", 
-                                     augment=AugmentBetasCam(device=torch.device('cuda'), t_z_range=[0, 0], t_xy_std=0), 
-                                     transform=data_transforms)
-    data = dataset.__getitem__(0)
-    print(data['height'])
-    print(data['betas'])
-    fig, axes = plt.subplots(1, 2)
-    # front_image = crop_resize_binary_image(data['front_image'])
-    axes[0].imshow(data["front_image"][0].cpu().numpy(), cmap='gray')
-    axes[1].imshow(data['side_image'][0].cpu().numpy(), cmap='gray')
-    plt.show()
+    dataset = BinaryImageMeasDataset(data_dir=config.GEN_TRAIN_DIR, 
+                                     transform=transforms.Compose([
+                                        ToTensor(),
+                                        BinTensor(threshold=0.5), 
+                                        Lambda(crop_true), 
+                                        Resize((512, 512), interpolation=F.InterpolationMode.NEAREST),
+                                    ]))
+    data_dict = dataset.__getitem__(0)
+    print()
