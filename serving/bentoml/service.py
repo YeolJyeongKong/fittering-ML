ebb68c6b1155b0f58385846d03f5dae54457199d
sinjy1203 <sinjy1203@gmail.com>
2023-07-28 17:43:31 +0900

    feat: deploy bentoml to AWS EC2

--- serving/bentoml/service.py
+++ serving/bentoml/service.py
@@ -29,12 +29,12 @@ svc = bentoml.Service(
     runners=[segmodel_runner, autoencoder_runner, regression_runner],
 )
 
-s3_access_key = pd.read_csv(paths.S3_ACCESS_KEY_PATH)
+# s3_access_key = pd.read_csv(paths.S3_ACCESS_KEY_PATH)
 s3 = boto3.client(
     "s3",
-    aws_access_key_id=s3_access_key["Access key ID"].values[0],
-    aws_secret_access_key=s3_access_key["Secret access key"].values[0],
-    region_name="ap-northeast-2",
+    # aws_access_key_id=s3_access_key["Access key ID"].values[0],
+    # aws_secret_access_key=s3_access_key["Secret access key"].values[0],
+    # region_name="ap-northeast-2",
 )
 BUCKET_NAME = "fittering-measurements-images"
 
@@ -54,12 +54,15 @@ def masking(input: ImageS3Path) -> ImageS3Path:
     front_masked_path = str(Path(front_path).parent / "front_masked.jpg")
     side_masked_path = str(Path(side_path).parent / "side_masked.jpg")
     front = Image.open(s3.get_object(Bucket=BUCKET_NAME, Key=front_path)["Body"])
-    side = Image.open(s3.get_object(Bucket=BUCKET_NAME, Key=front_path)["Body"])
+    front_size = front.size
+    side = Image.open(s3.get_object(Bucket=BUCKET_NAME, Key=side_path)["Body"])
+    side_size = side.size
 
     front = F.pil_to_tensor(front)
     front = F.resize(front, size=(512, 512))
     front = front.unsqueeze(dim=0).float() / 255.0
     front_masked = segmodel_runner.run(front)[0]
+    front_masked = F.resize(front_masked, size=front_size[::-1])
     front_masked = F.to_pil_image(front_masked).convert("RGB")
     buffered = BytesIO()
     front_masked.save(buffered, format="JPEG")
@@ -69,6 +72,7 @@ def masking(input: ImageS3Path) -> ImageS3Path:
     side = F.resize(side, size=(512, 512))
     side = side.unsqueeze(dim=0).float() / 255.0
     side_masked = segmodel_runner.run(side)[0]
+    side_masked = F.resize(side_masked, size=side_size[::-1])
     side_masked = F.to_pil_image(side_masked).convert("RGB")
     buffered = BytesIO()
     side_masked.save(buffered, format="JPEG")
