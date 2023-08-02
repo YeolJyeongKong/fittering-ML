4ba353c77de7c112ae77fd48a6aff1ba08d5e2c3
sinjy1203 <sinjy1203@gmail.com>
2023-08-02 16:55:18 +0900

    bug: jenkinsfile

--- jenkins/bentoml_build.sh
+++ jenkins/bentoml_build.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# ANACONDA_DIR="/home/shin/anaconda3"  # Anaconda가 설치된 디렉토리 경로
+# ENV_NAME="fittering-measurements-cpu"         # 가상환경 이름
+
+# # Anaconda가 설치된 디렉토리로 이동합니다.
+# cd $ANACONDA_DIR
+
+# # Anaconda를 활성화하는 스크립트를 실행합니다.
+# source bin/activate
+
+# # 원하는 가상환경을 활성화합니다.
+# conda activate $ENV_NAME
+
+
+
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate fittering-measurements-cpu
 bentoml build -f /home/shin/VScodeProjects/fittering-ML/outputs/2023-08-02/00-17-34/bentofile.yaml
