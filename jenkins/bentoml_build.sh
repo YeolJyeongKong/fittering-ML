786fca88a54f21de6e9fad1221bd0045e55f3d74
sinjy1203 <sinjy1203@gmail.com>
2023-08-02 15:08:00 +0900

    bug: jenkinsfile

---
+++ jenkins/bentoml_build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# source /home/shin/anaconda3/etc/profile.d/conda.sh
+conda activate fittering-measurements-cpu
+bentoml build -f /home/shin/VScodeProjects/fittering-ML/outputs/2023-08-02/00-17-34/bentofile.yaml
+bentoml containerize human_size_predict:latest
