c59d2d189d09518aad972c3a2f0e089c4891e5ef
sinjy1203 <sinjy1203@gmail.com>
2023-08-02 20:42:10 +0900

    bug: deploy shell script

--- jenkins/bentoml_deploy_ec2.sh
+++ jenkins/bentoml_deploy_ec2.sh
@@ -7,8 +7,8 @@ bentoml containerize human_size_predict:latest -t 210651441624.dkr.ecr.ap-northe
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 210651441624.dkr.ecr.ap-northeast-2.amazonaws.com
 docker push 210651441624.dkr.ecr.ap-northeast-2.amazonaws.com/human_size_predict:latest
 
-KEYPAIR = /home/shin/Documents/aws/keypairs/ubuntu-desktop-keypairs.pem
-REMOTE_HOST = ec2-54-180-148-94.ap-northeast-2.compute.amazonaws.com
-scp -i $KEYPAIR ./jenkins/deploy.sh ec2-user@$REMOTE_HOST:~/
-ssh -i $KEYPAIR ec2-user@$REMOTE_HOST "chmod +x deploy.sh"
-ssh -i $KEYPAIR ec2-user@$REMOTE_HOST ./deploy.sh
+KEYPAIR=/home/shin/Documents/aws/keypairs/ubuntu-desktop-keypairs.pem
+REMOTE_HOST=ec2-54-180-148-94.ap-northeast-2.compute.amazonaws.com
+scp -i $KEYPAIR ./jenkins/ec2_deploy.sh ec2-user@$REMOTE_HOST:~/
+ssh -i $KEYPAIR ec2-user@$REMOTE_HOST "chmod +x ec2_deploy.sh"
+ssh -i $KEYPAIR ec2-user@$REMOTE_HOST ./ec2_deploy.sh
--- jenkins/ec2_deploy.sh
+++ jenkins/ec2_deploy.sh
@@ -1,5 +1,6 @@
 DOCKER_IMAGE=210651441624.dkr.ecr.ap-northeast-2.amazonaws.com/human_size_predict
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 210651441624.dkr.ecr.ap-northeast-2.amazonaws.com
 docker pull $DOCKER_IMAGE
+docker stop $(docker ps -aq)
 docker ps -q --filter ancestor=$DOCKER_IMAGE | xargs -r docker stop
 docker run -i --rm -p 80:3000 $DOCKER_IMAGE serve
