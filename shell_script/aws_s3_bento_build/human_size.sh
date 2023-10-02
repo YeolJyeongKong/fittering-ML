7796378ab30b09d0da4e4febb6f94ab3a3e40fc4
sinjy1203 <sinjy1203@gmail.com>
2023-10-02 21:31:38 +0900

    feat: deploy fashion-cbf-svc using jenkins

---
+++ shell_script/aws_s3_bento_build/human_size.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate human-size-env
+bentoml build -f bentofile/sweep_best/bentofile_human_size.yaml
+bentoml export human_size_predict:latest s3://fittering-bento/human-size-predict.bento
