90b77a4e946e16b5c9b94e1b3cd38e13ce12ce64
sinjy1203 <sinjy1203@gmail.com>
2023-10-17 16:37:40 +0900

    feat: load bentomodel in jenkins

---
+++ jenkins/bentomodel_load.py
@@ -0,0 +1,21 @@
+import subprocess
+import argparse
+import bentoml
+from omegaconf import OmegaConf
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--bentofile-path", required=True)
+parser.add_argument("-s3", "--s3-dir", required=True)
+
+args = parser.parse_args()
+s3_dir = args.s3_dir
+bentofile_path = args.bentofile_path
+target_bentomodels = OmegaConf.load(bentofile_path).models
+
+for target_bentomodel in target_bentomodels:
+    model_name = target_bentomodel.split(":")[0]
+    try:
+        bentoml.models.get(target_bentomodel)
+    except bentoml.exceptions.NotFound:
+        bentoml.models.delete(model_name)
+        bentoml.models.import_model(f"{s3_dir}/{model_name}.bentomodel")
